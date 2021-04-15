d1d32464153f571f6d5ad75cb249ddb9fb6f2f3e
Alexandrebmx5 <alexandrebmx5@gmail.com>
2021-04-15 17:54:17 -0400

    Primeiro Commit

---
+++ Models/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcialAncelmo.Models
+{
+    public class Autor
+    {
+        public int id { get; set; }
+        public string linkedin { get; set; }
+        public string nome { get; set; }
+        
+
+        
+    }
+}
